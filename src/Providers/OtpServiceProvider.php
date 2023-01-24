8ff6f03d2209b90f3770185a29813d96ca318c96
Nurislan Umetaliev <nurislan.umetaliev@axelkg.com>
2023-01-24 11:50:20 +0600

    test

--- src/Providers/OtpServiceProvider.php
+++ src/Providers/OtpServiceProvider.php
@@ -12,11 +12,11 @@ class OtpServiceProvider extends ServiceProvider
     {
         $this->commands([
             InstallCommand::class,
-            MigrationsCommand::class
+//            MigrationsCommand::class
         ]);
 
         $this->publishes([__DIR__ . '/../../config/otp.php' => config_path('otp.php')], 'otp-config');
 
-        $this->publishes([__DIR__ . '/../../database/migrations' => database_path('migrations')], 'otp-migrations');
+//        $this->publishes([__DIR__ . '/../../database/migrations' => database_path('migrations')], 'otp-migrations');
     }
 }
